e4f9a230fa979fc3c8e0916183e264073063f66b
Morgane Vevert <mvevert@darkmatters.one>
2023-08-07 17:57:48 +0200

    MV blockout create kitsu asset

--- openpype/hosts/blender/plugins/publish/extract_blockout.py
+++ openpype/hosts/blender/plugins/publish/extract_blockout.py
@@ -28,12 +28,19 @@ class ExtractBlockout(publish.Extractor):
                 collection_data = {'COLLECTION': collection.name, 'OBJECT' : obj_array}
                 col_array.append(collection_data)
 
-        print("------BLOCKOUT EXTRACTEDDATA------")
-        for collection_data in col_array:
-            print("OBJ DATA = ", collection_data['COLLECTION'])
-            print("OBJ DATA = ", collection_data['OBJECT'])
-            print("OBJ DATA SET ", collection_data)
+            print("------BLOCKOUT EXTRACTEDDATA------")
+            for collection_data in col_array:
+                print("OBJ DATA = ", collection_data['COLLECTION'])
+                print("OBJ DATA = ", collection_data['OBJECT'])
+                print("OBJ DATA SET ", collection_data)
 
-        #print("KITSU PROJECT ", kitsu_cred.gazu.project.all_projects())
-        print("project = ", os.environ['AVALON_PROJECT'])
-        print('-----------------')
+                projects = kitsu_cred.gazu.project.get_project_by_name(os.environ['AVALON_PROJECT'])
+                type = kitsu_cred.gazu.asset.get_asset_type_by_name('Animation')
+                asset = kitsu_cred.gazu.asset.new_asset(
+                    projects,
+                    type,
+                    collection_data['COLLECTION'],
+                    "My asset description"
+                )
+                print("project = ", os.environ['AVALON_PROJECT'])
+                print('-----------------')
